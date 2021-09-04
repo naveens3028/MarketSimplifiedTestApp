a1a1eb6fc358631421a310f0ac9c0464f9f1fa46
naveens3028 <naveens3028@gmail.com>
2021-09-04 18:32:16 +0530

    Code optimization done

--- app/src/main/java/com/example/marketsimplifiedtestapp/helper/HomeTabViewAdapter.kt
+++ app/src/main/java/com/example/marketsimplifiedtestapp/helper/HomeTabViewAdapter.kt
@@ -14,7 +14,7 @@ import com.example.marketsimplifiedtestapp.ui.notifications.NotificationsFragmen
 class HomeTabViewAdapter(fm: FragmentActivity) : FragmentStateAdapter(fm) {
 
     override fun getItemCount(): Int {
-        return 5
+        return 3
     }
 
     override fun createFragment(position: Int): Fragment {
